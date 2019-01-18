90fdbe8cfd5f4e66d44da9cc3b3cf965ee1f6157
Rafael Jhonatan <rafael.jhonatan@squadra.com.br>
2019-01-18 11:28:59 -0200

    Adiciona teste controller person

--- src/test/java/com/bravi/contactlist/controllers/PersonControllerTest.java
+++ src/test/java/com/bravi/contactlist/controllers/PersonControllerTest.java
@@ -1,20 +1,41 @@
 package com.bravi.contactlist.controllers;
 
+import com.bravi.contactlist.services.PersonService;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.LinkedList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PersonControllerTest {
+    @InjectMocks
+    private PersonController personController;
+
+    @Mock
+    private PersonService personService;
+
     private MockMvc mockMvc;
 
-    @InjectMocks
-    PersonController personController;
-
     @Before
-    public void setUp() throws Exception {
-//        mockMvc = new MockMvcBuilder().build()
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(this.personController).build();
+    }
+
+    @Test
+    public void testFindAllPersons() throws Exception {
+        Mockito.when(personService.findAll()).thenReturn(new LinkedList<>());
+        mockMvc.perform(get("/persons")).andExpect(status().isOk()).andExpect(content().string("[]"));
     }
 }
